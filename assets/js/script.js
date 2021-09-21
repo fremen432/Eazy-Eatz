9c95b3ef07474ba44e5dc286773dc6c90fcd50b7
Mickey Phillips <phillipsmickey41@gmail.com>
2021-09-20 22:00:49 -0500

    DOM was not involved, but it now retrieves the ingredients of the recipe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,34 @@
 
-
 var getFood = function() {
-    var type = window.prompt("'cuisine(Italian)', 'excludeCuisine(Greek)', 'diet(vegetarian)'")
-    var search = window.prompt("italian");
-    var foodApiUrl = "https://api.spoonacular.com/recipes/complexSearch?" + type + "=" + search + "&apiKey=e00508acdc184205a22e718465e12ad6";
+    //var type = window.prompt("'cuisine', 'excludeCuisine', 'diet'")
+    //var search = window.prompt("italianI");
+    var recipe = window.prompt("RECIPE");
+    var foodApiUrl = "https://api.spoonacular.com/recipes/complexSearch?query=" + recipe + "&apiKey=e00508acdc184205a22e718465e12ad6";
     fetch(foodApiUrl)
     .then(function(response) {
         return response.json();
     })
-    .then(function(response) {
-        console.log(response);
+    .then(function(data) {
+        console.log(data);
+        console.log("Eazy Eatz Search: " + recipe);
+        var nutritionSearch = "true";
+        for(var i = 0; i < data.results.length; i++) {
+            console.log(data.results[i].title);
+        };
+        // adjust spoonId
+            // if spoon recipe is selected, it retrieves it id and places it in the link
+        var spoonId = data.results[0].id;
+        var infoApiUrl = "https://api.spoonacular.com/recipes/" + spoonId + "/information?includeNutrition=" + nutritionSearch + "&apiKey=e00508acdc184205a22e718465e12ad6";
+        fetch(infoApiUrl)
+        .then(function(response) {
+            return response.json();
+        }).then(function(information) {
+            console.log(information);
+            console.log(information.nutrition );
+            for (var i = 0; i < information.nutrition.ingredients.length; i++) {
+                console.log(information.nutrition.ingredients[i].name + " " + information.nutrition.ingredients[i].amount + " " + information.nutrition.ingredients[i].unit);
+            }
+        });
     });
 };
 
